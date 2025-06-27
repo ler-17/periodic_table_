c9cf20a96f4e04479f865b2bf5353c9a361b5663
Lauren Roberts <laurenroberts@Blue.local>
2025-06-27 08:06:02 -0700

    finished v3.

--- v3.py
+++ v3.py
@@ -17,41 +17,76 @@ def get_num(str):
 # inject number after each element
 # remove parenthesis
 # salvage code?
+
+
 # N(CHO)3Ca2
-def inject_in_parenthesis(substring, num = 1):
+def inject_in_substring(substring, multiple = 1):
     new_string = substring
     index = 0
-    for i in range(len(substring)):
-        if substring[i+1].isupper():
+
+    for i in range(len(substring) - 1):
+        if substring[i + 1].isupper():
             if not substring[i].isnumeric():
                 index += 1
-                new_string = new_string[:index] + str(num) + new_string[index:]
+                new_string = new_string[:index] + str(multiple) + new_string[index:]
             else:
-                index += 1
-                new_string = new_string[:index] + str(num) * new_string[index] + new_string[index + 1:]
+                num = int(multiple) * int(new_string[index])
+                new_string = new_string[:index] + str(num) + new_string[index + 1:]
         index += 1
 
+    if not substring[len(substring) - 1].isnumeric():
+        new_string = new_string + str(multiple)
+    else:
+        num = int(multiple) * int(new_string[index])
+        new_string = new_string[:index] + str(num)
 
-def make_monstrosity(species):
-    monstrosity = species
+    return new_string
 
+# now we have a function to deal with a substring. we just need to:
+# (1) call in strategic locations
+# (2)
+def parse_whole(species):
+    new_species = species
     while "(" in species:
-        start = species.indexOf("(")
-        end = species.indexOf(")")
-        num_index = end + 1
-        substring = species[start + 1: end ]
-        for i in range(len(substring)):
+        i_start = species.index("(")
+        i_end = species.index(")")
+        substring = species[i_start + 1: i_end]
+        multiple = species[i_end + 1]
 
+        if (i_end + 2) > len(species):
+            rest = ""
+        else:
+            rest = species[i_end + 2:]
 
+        new_species = species[:i_start] + inject_in_substring(substring, multiple) + rest
+        species = new_species
 
+    new_species = inject_in_substring(species, 1)
 
-    monstrosity_index = 0
-    for i in range(len(species) - 1):
-        if (species[i+1].isupper()) and not (species[i].isnumeric()):
-            monstrosity_index += 1
-            monstrosity = monstrosity[:monstrosity_index] + "1" + monstrosity[monstrosity_index:]
-        monstrosity_index += 1
+    return new_species
 
-    if not species[len(species) - 1].isnumeric():
-        monstrosity = monstrosity + "1"
-    return monstrosity
+print(parse_whole("H(CHO)2Ne"))
+
+def make_dic(monstrosity):
+    dic = {}
+    guy = 0
+    i = 0
+    while i < len(monstrosity):
+        if monstrosity[i].isnumeric():
+            num = get_num(monstrosity[i:])
+            dic[monstrosity[guy:i]] = dic.get(monstrosity[guy:i], 0) + num
+            guy = i + len(str(num))
+            i += len(str(num)) - 1
+        i += 1
+
+    return dic
+
+def get_molar_mass(dic, data):
+    molar_mass = 0
+    for element in dic:
+        molar_mass += data[element] * float(dic[element])
+    return molar_mass
+
+print("Enter species :D : ")
+species = input()
+print("Molar mass : " + str(get_molar_mass(make_dic(parse_whole(species)), data)))
